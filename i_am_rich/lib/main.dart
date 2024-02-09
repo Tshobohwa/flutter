6193d67316a2615cab367de97163dbafcdcb980a
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-09 23:39:40 +0200

    Add new icons

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Text("Hello World"),
-    ),
+    MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "I am rich",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey[200],
+      body: const Center(
+        child: Image(
+          image: AssetImage('assets/images/diamond.png'),
+        ),
+      ),
+    )),
   );
 }
