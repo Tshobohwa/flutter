d79e9d152b0aec1ef112a30932579db42eac3fca
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-13 23:31:48 +0200

    Add email card

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,43 +9,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         home: Scaffold(
       backgroundColor: Colors.teal,
       body: Column(
         children: <Widget>[
-          const CircleAvatar(
+          CircleAvatar(
             radius: 50.0,
             backgroundImage: NetworkImage(
                 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
           ),
-          const Text(
+          Text(
             "Salomon Tshobohwa",
             style: TextStyle(
                 fontFamily: 'Lobster', fontSize: 32, color: Colors.white),
           ),
-          const Text(
+          Text(
             "FLUTTER DEVELOPER",
             style: TextStyle(
                 fontFamily: 'Poppins', color: Colors.white, fontSize: 24),
           ),
-          Container(
-            height: 80,
-            margin: const EdgeInsets.all(10),
-            color: Colors.white,
-            child: const Row(
-              children: [
-                Icon(Icons.phone),
-                SizedBox(
-                  width: 10,
+          Card(
+              margin: EdgeInsets.all(10),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      "+243 97 34 58 999",
+                      style: TextStyle(fontSize: 18, color: Colors.teal),
+                    )
+                  ],
                 ),
-                Text(
-                  "+243 97 34 58 999",
-                  style: TextStyle(fontSize: 18, color: Colors.teal),
-                )
-              ],
-            ),
-          )
+              )),
+          Card(
+              margin: EdgeInsets.all(10),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: Row(
+                  children: [
+                    Icon(Icons.email),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      "salomon@gmail.com",
+                      style: TextStyle(fontSize: 18, color: Colors.teal),
+                    )
+                  ],
+                ),
+              ))
         ],
       ),
     ));
